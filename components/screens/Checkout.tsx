a11ae35c5b2cfcb98e0106e3b4f869b24c6a86e1
Victor Atencio <atenciovictoralejandro@gmail.com>
2025-06-29 10:59:58 -0400

    Add Google Maps iframe to Contact and Checkout pages for location display

--- components/screens/Checkout.tsx
+++ components/screens/Checkout.tsx
@@ -494,6 +494,25 @@ ${customerInfo.notes ? `*Notas adicionales:* ${customerInfo.notes}` : ""}
 								</div>
 							</CardContent>
 						</Card>
+
+						<Card className="bg-stone-950 border-none">
+							<CardHeader>
+								<CardTitle className="text-white">Nuestra dirección</CardTitle>
+							</CardHeader>
+							<CardContent>
+								<div className="space-y-4">
+									<iframe
+										src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d304.14812751061334!2d-71.61309633999593!3d10.680753238250476!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e8999f64652e995%3A0x51e57ea5f97d3196!2sBurgersmokemcbo!5e0!3m2!1ses-419!2snl!4v1751208723721!5m2!1ses-419!2snl"
+										width="100%"
+										height="350"
+										style={{ border: 0 }}
+										allowFullScreen={true}
+										loading="lazy"
+										referrerPolicy="no-referrer-when-downgrade"
+									></iframe>
+								</div>
+							</CardContent>
+						</Card>
 					</div>
 
 					{/* Right Column - Customer Information and Payment Upload */}
